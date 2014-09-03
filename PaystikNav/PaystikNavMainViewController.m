4bafaef217bc73050e14aa4c5b8c9b32af1257cf
Bobie <bobie@herxun.co>
2014-09-03 15:19:11 -0700

    init commit

---
+++ PaystikNav/PaystikNavMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  PaystikNavMainViewController.m
+//  PaystikNav
+//
+//  Created by Bobie Chen on 9/3/14.
+//  Copyright (c) 2014 Bobie Chen. All rights reserved.
+//
+
+#import "PaystikNavMainViewController.h"
+
+@interface PaystikNavMainViewController ()
+
+@end
+
+@implementation PaystikNavMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
